14e8ff5857d14530cc804e62162337555e40f77b
Николай Стрельников <898894951342013@mail.ru>
2023-09-30 15:58:05 +0300

    Добавлено приветствие в разное время

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  Println('Привет, мир!');
+  var h := ReadInteger('Введи какой сейчас час');
+  Assert(h in 0..23, 'От 0 до 23');
+  case h of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вычер, мир!')
+  else
+      Println('Доброй ночи, мир!');
+  end;
 end.
