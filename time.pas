7030cca858b1b73b88cd89fef2a84c7bce938c46
Николай Стрельников <898894951342013@mail.ru>
2023-09-30 16:03:43 +0300

    Добавлено кол-во дней

--- time.pas
+++ time.pas
@@ -2,7 +2,7 @@
   var y := ReadInteger('Введите год');
   Assert(y > 0, 'Введи год');
   if (y mod 4 = 0) and (y mod 100 <> 0) or (y mod 400 = 0) then
-    Println('Високосный год')
+    Println('Високосный год - 366 дней')
   else
-    Println('Не високосный год');
+    Println('Не високосный год - 365 дней');
 end.
